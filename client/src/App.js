7a57bacee1c2921b681593cbcb14bd0523fb8322
lky <lkymykiki@nate.com>
2020-05-21 18:40:54 +0900

    Add mysql

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,31 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3) * 3,
     overflowX: 'auto'
   },
   table: {
     minWidth:1080
+  },
+  progress:{
+    margin: theme.spacing(2) * 2
   }
 });
 
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   }
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -35,6 +41,11 @@ class App extends Component {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render(){
@@ -66,7 +77,12 @@ class App extends Component {
                     job={c.job}
                   />
                 );
-              }) : ""
+              }) : 
+              <TableRow>
+                <TableCell colspan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const experss = require('express');
 const bodyParser = require('body-parser');
 const app = experss();
@@ -6,34 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
 app.get('/api/customers', (req, res) =>{
-    res.send([
-        {
-        'id':1,
-        'image': 'https://placeimg.com/64/64/any ',
-        'name': '이정후',
-        'birthday': '20200521',
-        'gender': '남자',
-        'job': '키움넥센'
-        },
-        {
-          'id':2,
-          'image': 'https://placeimg.com/64/64/2 ',
-          'name': '안치홍',
-          'birthday': '20200521',
-          'gender': '남자',
-          'job': '롯데자이언츠'
-        },
-        {
-          'id':3,
-          'image': 'https://placeimg.com/64/64/3 ',
-          'name': '강백호',
-          'birthday': '20200521',
-          'gender': 'kt위즈',
-          'job': '대학생'
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-          
-      ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
