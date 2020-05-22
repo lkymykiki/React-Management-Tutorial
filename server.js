33c9c455ff7bf0b9f8df54f1b991559eab242aca
lky <lkymykiki@nate.com>
2020-05-22 15:30:47 +0900

    등록 개발

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const experss = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = experss();
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -20,6 +20,9 @@ const connection = mysql.createConnection({
     database: conf.database
 });
 connection.connect();
+
+const multer = require('multer');
+const upload = multer({dest: './upload'});
 app.get('/api/customers', (req, res) =>{
     connection.query(
         "SELECT * FROM customer",
@@ -28,5 +31,22 @@ app.get('/api/customers', (req, res) =>{
         }
     );
 });
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) =>{
+    let sql = 'INSERT INTO customer VALUES(null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
